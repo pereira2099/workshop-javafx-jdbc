93957ac2a00a54426932fa0ac4608203859312c1
Lucas Pereira <pereirapas123@gmail.com>
2020-04-28 19:48:23 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -27,6 +27,14 @@ public class Utils {
 			return null;
 		}
 	}
+	
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 	// https://stackoverflow.com/questions/47484280/format-of-date-in-the-javafx-tableview
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
